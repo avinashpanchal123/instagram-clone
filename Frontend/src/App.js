0159080b61bec0d5596a11ae400b7232bb9fc892
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2022-01-21 14:26:45 +0530

    add

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,21 +1,18 @@
 import { Messanger } from "./MainChat/Messanger/ChatMessanger"
-//import socketIO from "socket.io-client";
 
 import "./App.css"
-// const ENDPOINT = "http://localhost:4500/"
-// const socket = socketIO(ENDPOINT , {transport:["websocket"]} )
+import { ProfilePage } from "./ProfilePage/ProfilePage"
+
 
 const App = ()=>{
  
-//     socket.on("connect" ,()=>{
 
-//    })
     return (
         <div>
             <div className="fakeNavebar">Navbar</div>
 
-        
-           <Messanger/>
+              <ProfilePage/>
+           
         </div>
         
     )
---
+++ Frontend/src/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,16 @@
+import "./ProfilePage.css"
+
+
+const ProfilePage=()=>{
+    return(
+       
+        <div className="MainContainer">
+            <div  className="ProfileDetails" >
+                <div className="ProfilePhoto">profile photo</div>
+                <div className="ProfileDetails">profileDetails</div>
+            </div>
+        </div>
+    )
+}
+
+export {ProfilePage}
