89287aa662744607ec710229ee22d680490cc699
Dipankar Sarkar <sarkar.dipankar9999@gmail.com>
2022-01-23 02:11:11 +0530

    server

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const User = require ("./models/user.model");
+// app.use(cors())
 
-app.use(cors())
-
-app.use(express.json());
+ app.use(express.json());
 app.use((req, res, next) => {
     
-    res.header({"Access-Control-Allow-Origin": "*","Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"});
+    res.header({"Access-Control-Allow-Origin": "*"});
     
     next();
   }) 
+
+  app.get("/user/:id", async (req,res)=>{
+    try{
+        const user = await User.findById(req.params.id).lean().exec();
+        return res.send(user);
+
+    }catch(e){
+        return res.status(400).json({ status: "failed", message: e.message });
+    }
+})
 
 const {register,login} = require ("./controllers/user.controller");
 
