af875d189262ee83084953eb7c8d2c8a6fa44fa4
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2022-01-23 17:37:43 +0530

    username

--- Frontend/src/MainChat/Messanger/ChatMessanger.jsx
+++ Frontend/src/MainChat/Messanger/ChatMessanger.jsx
@@ -15,6 +15,8 @@ let socket
 
 export const Messanger =()=>{
 
+    const [user, setUser] = useState([]);
+
     const [chosenEmoji, setChosenEmoji] = useState(null);
 
     const onEmojiClick = (event, emojiObject) => {
@@ -81,6 +83,21 @@ useEffect(() => {
       socket.off()  
     }
 }, [messages])
+
+useEffect(() => {
+    let token = JSON.parse (localStorage.getItem ('user'));
+    console.log(token);
+    
+
+    fetch(`http://localhost:3005/user/${token[0]}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        console.log(data);
+      })
+    }, []);
+ console.log("user:",user);
+
     return <>
     <Navbar/>
     <div className="messenger">
@@ -89,7 +106,7 @@ useEffect(() => {
              
              <div className="menuBox1">
                  <div className="namebox" >
-                      <div className="personName">vinaymakade</div>
+                      <div className="personName">{user.username}</div>
                   
                       <span style={{display:" inline-block",transform: "rotate(180deg)"}}>
                            <svg aria-label="Down Chevron Icon" class="_8-yf5 " color="#262626" fill="#262626" height="20" role="img" viewBox="0 0 24 24" width="20"><path d="M21 17.502a.997.997 0 01-.707-.293L12 8.913l-8.293 8.296a1 1 0 11-1.414-1.414l9-9.004a1.03 1.03 0 011.414 0l9 9.004A1 1 0 0121 17.502z"></path></svg>
